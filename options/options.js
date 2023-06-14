53c5f372ffd36493bd1334878ddd64097d28fd22
Crowdbotics <engineering@crowdbotics.com>
2023-06-14 13:24:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-recipe-de-91536",
+  url: "https://sparkling-recipe-de-91536.botics.co",
+  api: "https://sparkling-recipe-de-91536.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
